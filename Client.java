c46b146e232731aa9864b599fbde8a8f3ed713bd
Vibolroath Leav <vibolroath.leav@students.mq.edu.au>
2023-04-03 14:19:08 +1000

    find largest server based on core & put in arrlist

--- Client.java
+++ Client.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Client {
   public static void main(String[] args) throws IOException {
@@ -9,6 +10,12 @@ public class Client {
     DataOutputStream dout = new DataOutputStream(s.getOutputStream());
     String userName = System.getProperty("user.name");
     String response;
+    boolean jobAvail = false;
+    String largestServerType = " ";
+    int largestServerCores = 0;
+    int largestServerID;
+    int largestServerCount = 0;
+    ArrayList<String> largestServers = new ArrayList<String>();
 
     // send handshake message to the server
     dout.write(("HELO\n").getBytes());
@@ -23,6 +30,48 @@ public class Client {
     dout.flush();
     response = bin.readLine();
 
+    while (!response.equals("NONE")) {
+      String[] jobDetails = response.split(" ");
+      int jobID = Integer.parseInt(jobDetails[2]);
+
+      if (response.startsWith("JOBN")) {
+        dout.write(("GETS All\n").getBytes());
+        dout.flush();
+        response = bin.readLine();
+
+        String[] dataLoop = response.split(" ");
+        int nServer = Integer.parseInt(dataLoop[1]);
+
+        dout.write(("OK\n").getBytes());
+        dout.flush();
+
+        if (jobAvail == true) {
+          for (int x = 0; x < nServer; x++) {
+            response = bin.readLine();
+          }
+        } else if (jobAvail == false) {
+          for (int y = 0; y < nServer; y++) {
+            response = bin.readLine();
+            largestServers.add(response);
+
+            String[] serverDetails = response.split(" ");
+            String serverType = serverDetails[0];
+            int serverID = Integer.parseInt(serverDetails[1]);
+            int serverCores = Integer.parseInt(serverDetails[4]);
+
+            if (serverCores > largestServerCores) {
+              largestServerType = serverType;
+              largestServerCores = serverCores;
+              largestServerID = serverID;
+            }
+          }
+        }
+      } else if (response.startsWith("JCPL")) {
+        dout.write(("REDY\n").getBytes());
+        dout.flush();
+        response = bin.readLine();
+      }
+    }
     dout.write(("QUIT\n").getBytes());
     dout.flush();
     response = bin.readLine();
@@ -30,6 +79,6 @@ public class Client {
     dout.close();
     bin.close();
     s.close();
-  
+
   }
 }
