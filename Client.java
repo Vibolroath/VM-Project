8f1cbcb095bff2c50ab8bdca15338a89763b440c
Vibolroath Leav <vibolroath.leav@students.mq.edu.au>
2023-04-03 15:40:19 +1000

    added comments to Client.java files

--- Client.java
+++ Client.java
@@ -18,34 +18,43 @@ public class Client {
     int largestServerAvail = 0;
     ArrayList<String> largestServers = new ArrayList<String>();
 
-    // send handshake message to the server
+    // send handshake message to the ds-server
+    // send HELO to ds-server and read the response
     dout.write(("HELO\n").getBytes());
     dout.flush();
     response = bin.readLine();
 
+    // send AUTH with username to ds-server and read the response
     dout.write(("AUTH " + userName + "\n").getBytes());
     dout.flush();
     response = bin.readLine();
 
+    // send REDY to ds-server and read the response 
     dout.write(("REDY\n").getBytes());
     dout.flush();
     response = bin.readLine();
 
     while (!response.equals("NONE")) {
+      // array of strings containing JOBN and its details from ds-server
       String[] jobDetails = response.split(" ");
+      // jobID is located on the second index of JOBN
       int jobID = Integer.parseInt(jobDetails[2]);
 
       if (response.startsWith("JOBN")) {
+        // send GETS ALL to ds-server to get DATA and read the response
         dout.write(("GETS All\n").getBytes());
         dout.flush();
         response = bin.readLine();
 
+        // array of strings containing DATA and its details from ds-server
         String[] dataLoop = response.split(" ");
         int nServer = Integer.parseInt(dataLoop[1]);
 
+        // send OK to server
         dout.write(("OK\n").getBytes());
         dout.flush();
 
+        // if there are jobs available, read the response. If not, add the response to arraylist
         if (jobAvail == true) {
           for (int x = 0; x < nServer; x++) {
             response = bin.readLine();
@@ -55,11 +64,14 @@ public class Client {
             response = bin.readLine();
             largestServers.add(response);
 
+            // array of strings containing Server and its details from ds-server
             String[] serverDetails = response.split(" ");
+            // assigning server type, id, cores based on their index location of server
             String serverType = serverDetails[0];
             int serverID = Integer.parseInt(serverDetails[1]);
             int serverCores = Integer.parseInt(serverDetails[4]);
 
+            // finding largest server based on cores
             if (serverCores > largestServerCores) {
               largestServerType = serverType;
               largestServerCores = serverCores;
@@ -68,6 +80,7 @@ public class Client {
           }
         }
 
+        // counting available largest server
         if (jobAvail == false) {
           for (int x = 0; x < largestServers.size(); x++) {
             String[] serList = largestServers.get(x).split(" ");
@@ -78,34 +91,39 @@ public class Client {
             }
           }
         }
+        // send OK to ds-server and read the response
         dout.write(("OK\n").getBytes());
         dout.flush();
         response = bin.readLine();
 
+        //use modulo to find largestserverid and start scheduling the jobs
         schedJob = jobID % largestServerAvail;
         largestServerID = schedJob;
+        // send SCHD to ds-server to schedule jobs and read the response
         dout.write(("SCHD " + jobID + " " + largestServerType + " " + largestServerID + "\n").getBytes());
         dout.flush();
         response = bin.readLine();
 
+        // send REDY to ds-server and read the response 
         dout.write(("REDY\n").getBytes());
         dout.flush();
         response = bin.readLine();
 
       } else if (response.startsWith("JCPL")) {
+        // send REDY to ds-server and read the response 
         dout.write(("REDY\n").getBytes());
         dout.flush();
         response = bin.readLine();
       }
     }
-
+    // send QUIT to ds-server and read the response 
     dout.write(("QUIT\n").getBytes());
     dout.flush();
     response = bin.readLine();
 
+    //closing the output-stream, input-stream, and socket
     dout.close();
     bin.close();
     s.close();
-
   }
 }
